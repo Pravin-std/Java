f259602c1a3d06955f7a1e1ee4cb56c96ef5b230
Pravin-std <pravinstark456@gmail.com>
2026-01-15 10:00:00 +0530

    Day 1 - pb42

---
+++ 1D Array/pb42.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class PB42 {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+
+        Arrays.sort(arr);
+
+        System.out.println("Kth Largest = " + arr[arr.length - k]);
+    }
+}
