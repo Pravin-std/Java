177ccb7c619149bc72a66b6af7b038fb6a8bf8a5
Pravin-std <pravinstark456@gmail.com>
2026-01-21 10:00:00 +0530

    Day 1 - pb48

---
+++ 1D Array/pb48.java
@@ -0,0 +1,14 @@
+public class PB48 {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) count++;
+            }
+        }
+
+        System.out.println("Inversions = " + count);
+    }
+}
