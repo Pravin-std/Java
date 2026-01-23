83a2c8aaf3eaf41ed82f3e7e586370e30f861e32
Pravin-std <pravinstark456@gmail.com>
2026-01-23 10:00:00 +0530

    Day 1 - pb50

---
+++ 1D Array/pb50.java
@@ -0,0 +1,23 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class PB50 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+
+        Deque<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!dq.isEmpty() && dq.peek() < i - k + 1) dq.poll();
+
+            while (!dq.isEmpty() && arr[dq.peekLast()] < arr[i]) dq.pollLast();
+
+            dq.offer(i);
+
+            if (i >= k - 1) {
+                System.out.print(arr[dq.peek()] + " ");
+            }
+        }
+    }
+}
