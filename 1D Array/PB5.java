accc475dd34b7c88715b6c7469259efbbfe93a97
Pravin-std <pravinstark456@gmail.com>
2026-03-01 10:00:00 +0530

    Added PB5.java (1D Array)

---
+++ 1D Array/PB5.java
@@ -0,0 +1,15 @@
+public class PB5 {
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 30, 90, 20};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Min = " + min);
+    }
+}
