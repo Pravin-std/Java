3e0f532c0705b5565f4209760a471c7bafa173a7
Pravin-std <pravinstark456@gmail.com>
2026-03-06 10:00:00 +0530

    Added PB23.java (1D Array)

---
+++ 1D Array/PB23.java
@@ -0,0 +1,13 @@
+public class PB23 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5};
+        int n = 5;
+
+        int sum = n * (n + 1) / 2;
+        int arrSum = 0;
+
+        for (int num : arr) arrSum += num;
+
+        System.out.println("Missing = " + (sum - arrSum));
+    }
+}
