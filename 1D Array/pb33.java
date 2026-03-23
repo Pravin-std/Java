899362517900278dc062627d880488cc61d55da5
Pravin-std <pravinstark456@gmail.com>
2026-03-23 10:00:00 +0530

    Day 1 - pb33

---
+++ 1D Array/pb33.java
@@ -0,0 +1,14 @@
+public class PB33 {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1, 2, 3, 2, 2};
+
+        int count = 0, candidate = 0;
+
+        for (int num : arr) {
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        System.out.println("Majority = " + candidate);
+    }
+}
