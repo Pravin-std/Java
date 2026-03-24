b5930954aa8fd4c86c3130425229b241658e598b
Pravin-std <pravinstark456@gmail.com>
2026-03-24 10:00:00 +0530

    Day 1 - pb34

---
+++ 1D Array/pb34.java
@@ -0,0 +1,11 @@
+public class PB34 {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0};
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                System.out.println("Peak = " + arr[i]);
+            }
+        }
+    }
+}
