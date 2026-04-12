ea999a00cbcb8ab0f37dfa1dd9dcf163752cb004
pravin <pravinstark456@gmail.com>
2026-04-12 10:00:00 +0530

    Add pb19.java

---
+++ 2d Array/pb19.java
@@ -0,0 +1,23 @@
+A = [[1,2,3],[4,5,6],[7,8,9]]
+
+top, bottom = 0, len(A)-1
+left, right = 0, len(A[0])-1
+
+while top <= bottom and left <= right:
+    for i in range(left, right+1):
+        print(A[top][i], end=" ")
+    top += 1
+
+    for i in range(top, bottom+1):
+        print(A[i][right], end=" ")
+    right -= 1
+
+    if top <= bottom:
+        for i in range(right, left-1, -1):
+            print(A[bottom][i], end=" ")
+        bottom -= 1
+
+    if left <= right:
+        for i in range(bottom, top-1, -1):
+            print(A[i][left], end=" ")
+        left += 1
